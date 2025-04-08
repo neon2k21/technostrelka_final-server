ba7383b4bffc4d8ffd24c8a5eeef893162fc7481
Chirushka <ogloblya2009@gmail.com>
2025-04-09 01:28:37 +0300

    квизы

--- routes/kviz.routes.js
+++ routes/kviz.routes.js
@@ -6,6 +6,6 @@ router.post('/createKviz', KvizController.createKviz)
 router.get('/getKviz', KvizController.getKviz)
 router.delete('/deleteKviz', KvizController.deleteKviz)
 router.post('/completeKviz', KvizController.completeKviz)
-router.get('/getSplashKviz', KvizController.getSplashKviz)
+router.post('/getSplashKviz', KvizController.getSplashKviz)
 router.get('/getAllKviz', KvizController.getAllKviz)
 module.exports = router
