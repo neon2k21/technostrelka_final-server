c206922600e58892b403693a4836a5f8a1b2bf10
Chirushka <ogloblya2009@gmail.com>
2025-04-08 14:20:57 +0300

    Схема бд

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
-const objectRouter = require('./routes/object.routes')
-const publicationsRouter = require('./routes/publications.routes')
+//const objectRouter = require('./routes/object.routes')
+//const publicationsRouter = require('./routes/publications.routes')
 
 const bodyParser = require('body-parser');
 
@@ -11,8 +11,8 @@ const app = express()
 
 app.use(bodyParser.json({limit: '500mb'}))
 app.use('/api',userRouter)
-app.use('/api',objectRouter)
-app.use('/api',publicationsRouter)
+app.use('/api',)
+app.use('/api',)
 
 
 var admin = require("firebase-admin");
